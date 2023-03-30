72ec0ed10a82fd5c498f1bff833158eaf042b14c
nespolisimmaco <snespoli@yahoo.com>
2023-03-30 15:48:38 +0200

    Add: array of objects created

--- js/script.js
+++ js/script.js
@@ -3,7 +3,32 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            
+            toDoInput: "",
+            toDo: [
+                {
+                    text: "Svolgere l'esercizio",
+                    done: true
+                },
+                {
+                    text: "Comprare il pane",
+                    done: false
+                },
+                {
+                    text: "Portare a spasso il cane",
+                    done: true
+                },
+                {
+                    text: "Andare in palestra",
+                    done: false
+                },
+                {
+                    text: "Chiamare i nonni",
+                    done: true
+                },
+            ]
         }
+    },
+    methods: {
+        
     }
 }).mount("#app");
